cbb8958417574dcce772bfb587c02e1a4c80dce8
israel <iortega@dgp.unam.mx>
2016-06-28 13:44:15 -0500

    [israel,27/06/2016 17:47] Alta admin

--- models/user.js
+++ models/user.js
@@ -1,16 +1,18 @@
 /**
-*Libreria user.js
-Libreria que contiene la información necesaria para crear un objeto de tipo mongoose
-necesario para poder trabajar con conexiones hacia mongodb
+*Biblioteca user.js
+Biblioteca que contiene la información necesaria para crear un objeto de tipo mongoose
 */
+
 //Llamando a mongoose, que nos ayuda a gestionar el objeto
 var mongoose = require('mongoose');
 //ip o direción en donde tenemos nuestra base de datos
 mongoose.connect('mongodb://localhost/usuarios');
 //inicializamos nuestro esquema
 var Schema = mongoose.Schema;
+
 //variable para decirle a valor sexo cuales son sus dos posibles valores
 var posibles_valores=["M","F"];
+//
 //recordemos que nuestro esquema es la forma en que le indicamos a mongoose como
 //enviar los datos hacia la base de datos.
 var user_schema = new Schema({
@@ -23,9 +25,10 @@ var user_schema = new Schema({
 			},
 	usuario:String,
 	admin:Boolean
+        
 });
 
-var puesto_schema = new Schema({
+var vacante_schema = new Schema({
 	id:Number,
 	nombre:String,
 	descrip:String,
@@ -36,7 +39,7 @@ var puesto_schema = new Schema({
 
 //aquí inicializamos la variable user
 var User = mongoose.model("User", user_schema);
-var Puesto = mongoose.model("Puesto", puesto_schema)
+var Vacante = mongoose.model("Vacante", vacante_schema)
 //y aquí se envia la variable hacia el export
 module.exports.User = User;
-module.exports.Puesto = Puesto;
+module.exports.Vacante = Vacante;
